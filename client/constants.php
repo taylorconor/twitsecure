e610f75d4f4e4892621cc731fcdf2573e7f49875
Conor Taylor <taylorconor@me.com>
2015-03-29 19:52:19 +0100

    Cleaning files

--- client/constants.php
+++ client/constants.php
@@ -8,7 +8,6 @@
 
 // client's secret, also known as 'x'
 define("SECRET", 6);
-//define("KEYAUTHORITY", "http://localhost/twitsecure/keyAuthority");
 define("KEYAUTHORITY", "http://taylorconor.com/twitsecure/keyAuthority");
 
 // group leader
--- client/index.php
+++ client/index.php
@@ -38,8 +38,8 @@ $url = $connection->url('oauth/authorize',
 <html>
 <head>
 	<title>twitsecure</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<script src="js/bootstrap.min.js"></script>
 </head>
 <body>
 <div class="container text-center">
